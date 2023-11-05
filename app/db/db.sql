371d6574556d185ae08d3d218a2c251752b17440
afelro97 <afelro97@gmail.com>
2023-11-04 20:59:43 -0500

    Get tasks and  create functions

---
+++ app/db/db.sql
@@ -0,0 +1,8 @@
+-- table tasks
+CREATE TABLE tasks (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(200) NOT NULL,
+    description VARCHAR(200),
+    done BOOLEAN NOT NULL DEFAULT 0,
+    createAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
--- app/server/controllers/tasks.controllers.js
+++ app/server/controllers/tasks.controllers.js
@@ -1,14 +1,41 @@
-export const getTasks = (req, res) => {
-    res.send("obteniendo tareas")
-}
+import {pool} from '../../db/db.js';
 
-export const getTask = (req, res) => {
-    res.send("obteniendo una tarea")
-}
+export const getTasks = async(req, res) => {
+    const [result] = await pool.query(
+        "SELECT * FROM tasks ORDER BY createAt ASC"
+    );
+    res.json(result)
+    console.log(result);
+};
 
-export const createTask = (req, res) => {
-    res.send("creando tareas")
-}
+export const getTask = async (req, res) => {
+    try {
+      const [result] = await pool.query("SELECT * FROM tasks WHERE id = ?", [
+        req.params.id,
+      ]);
+  
+      if (result.length === 0)
+        return res.status(404).json({ message: "Task not found" });
+  
+      res.json(result[0]);
+    } catch (error) {
+      return res.status(500).json({ message: error.message });
+    }
+  };
+
+
+export const createTask = async(req, res) => {
+    const { title, description } = req.body;
+    const [result] = await pool.query(
+        "INSERT INTO tasks(title, description) VALUES (?, ?)", 
+        [title, description]
+    );
+    res.json({
+        id: result.insertId,
+        title,
+        description,
+    });
+};
 
 export const updateTask = (req, res) => {
     res.send("actualizando tarea")
