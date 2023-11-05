b89e376f24292884319b827c78a7956572bc5581
afelro97 <afelro97@gmail.com>
2023-11-04 21:04:12 -0500

    Delete

--- app/server/controllers/tasks.controllers.js
+++ app/server/controllers/tasks.controllers.js
@@ -41,6 +41,12 @@ export const updateTask = (req, res) => {
     res.send("actualizando tarea")
 }
 
-export const deleteTask = (req, res) => {
-    res.send("eliminando tarea")
+export const deleteTask = async(req, res) => {
+    const [result] = await pool.query('DELETE FROM tasks WHERE id = ?', [req.params.id]);
+    
+    if (result.affectedRows === 0) {
+        return res.status(404).json({ message: "Task not found" });
+    }
+
+    return res.sendStatus(204);
 }
