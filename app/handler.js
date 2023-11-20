e398b3fb1bce0a3077aa8f49efbcac4817de1fcc
Andres Rodriguez <afelro97@gmail.com>
2023-11-20 13:40:43 -0500

    Pruebas de cors

--- app/handler.js
+++ app/handler.js
@@ -1,4 +1,4 @@
 import serverless from 'serverless-http';
-import app from './server/index.js';  // Asegúrate de que el camino es correcto
+import app from './server/index.js';
 
 export const handler = serverless(app);
--- app/server/index.js
+++ app/server/index.js
@@ -1,15 +1,31 @@
 import express from 'express';
+import cors from 'cors';
 import { PORT } from '../server/config.js';
 import indexRoutes from './routes/index.routes.js';
 import tasksRoutes from './routes/tasks.routes.js';
 
 const app = express();
 
+// Opciones de CORS
+const corsOptions = {
+  origin: 'http://localhost:5173', 
+  optionsSuccessStatus: 200,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+};
+
+
+app.use(cors(corsOptions));
+
+
 app.use(express.json());
+
+
 app.use(indexRoutes);
 app.use(tasksRoutes);
 
-app.listen(PORT);
-console.log(`Server is listening on port ${PORT}`);
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
 
 export default app;
